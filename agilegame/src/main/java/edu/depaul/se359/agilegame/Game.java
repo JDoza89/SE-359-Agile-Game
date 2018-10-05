8bd2c3fdf6a25104758b1e5a7b14cb4f3279ac5d
Ethan Lin <ethan.lin@uwex.edu>
2018-10-05 11:59:00 -0500

    added simple game manager and game state test in main
    output result in console

--- agilegame/src/main/java/edu/depaul/se359/agilegame/Game.java
+++ agilegame/src/main/java/edu/depaul/se359/agilegame/Game.java
@@ -1,7 +1,18 @@
 package edu.depaul.se359.agilegame;
 
 public class Game {
+
     public static void main(String[] args) {
-        System.out.print("This is the game");
+
+        GameManager.getInstance().startGame();
+        System.out.println(GameManager.getInstance().getGameState());
+
+        GameManager.getInstance().playGame();
+        System.out.println(GameManager.getInstance().getGameState());
+
+        GameManager.getInstance().endGame();
+        System.out.println(GameManager.getInstance().getGameState());
+
     }
+
 }
