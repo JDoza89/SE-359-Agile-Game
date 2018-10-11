f4c75c8298b809d818585a559ab14220ede3cd7c
Ethan Lin <ethan.lin@uwex.edu>
2018-10-11 16:35:02 -0500

    some simple team, player, and team manager test (will be remove later)

--- agilegame/src/main/java/edu/depaul/se359/agilegame/Game.java
+++ agilegame/src/main/java/edu/depaul/se359/agilegame/Game.java
@@ -9,7 +9,30 @@ public class Game {
     public static void main(String[] args) {
 
         // start the game when program runs
-        GameManager.getInstance().startGame();
+        //GameManager.getInstance().startGame();
+
+        Team teamOne = new Team();
+        Player scrumMaster = new ScrumMasterPlayer();
+
+        System.out.println(teamOne.getId());
+        System.out.println(scrumMaster.getId());
+
+
+        TeamManager.getInstance().addTeam(teamOne);
+        TeamManager.getInstance().addPlayerToTeam(teamOne.getId(),scrumMaster);
+        System.out.println(TeamManager.getInstance().getPlayer(teamOne.getId(),scrumMaster.getId()));
+
+
+
+        Team teamTwo = new Team();
+        Player developer = new DeveloperPlayer();
+
+        System.out.println(teamTwo.getId());
+        System.out.println(developer.getId());
+
+        TeamManager.getInstance().addTeam(teamTwo);
+        TeamManager.getInstance().addPlayerToTeam(teamTwo.getId(),developer);
+        System.out.println(TeamManager.getInstance().getPlayer(teamTwo.getId(),developer.getId()));
 
     }
 
