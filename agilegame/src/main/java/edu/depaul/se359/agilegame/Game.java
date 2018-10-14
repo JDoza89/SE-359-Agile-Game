1b5dcfffee8bf3f22560cae3d400e63faab62e7a
Jack Chai <yuchaiyuchai@gmail.com>
2018-10-14 00:14:44 -0500

    Autoread all files in the deck directory
    +Added auto read functionality
    - Need to clean code later

--- agilegame/src/main/java/edu/depaul/se359/agilegame/Game.java
+++ agilegame/src/main/java/edu/depaul/se359/agilegame/Game.java
@@ -6,9 +6,11 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Objects;
 
 /*
     Parse the JSON in Main.
@@ -28,28 +30,36 @@ public class Game {
         System.out.println("Working Directory = " +
                 System.getProperty("user.dir"));
 
-        // grab whole file
-        JSONObject jsonObject = (JSONObject)parser.parse(
-                new InputStreamReader(Game.class.
-                        getResourceAsStream("/decks/storyDeck.json")));        //System.out.println(jsonObject);
+        File folder = new File(System.getProperty("user.dir") + "/src/main/resources/decks");
+        File[] listOfFiles = folder.listFiles();
 
-        // loop file with array
+        assert listOfFiles != null;
+        for (File file : listOfFiles) {
+            if (file.isFile())
+            {
+                // grab whole file
+                JSONObject jsonObject = (JSONObject)parser.parse(
+                        new InputStreamReader(Game.class.
+                                getResourceAsStream("/decks/" + file.getName())));
 
-        JSONArray jsonArray = (JSONArray) jsonObject.get("Cards");
-        JSONObject currObj = (JSONObject) jsonArray.get(0);
-        String cardType = (String) currObj.get("Type");
-        //System.out.println(cardType);
+                JSONArray jsonArray = (JSONArray) jsonObject.get("Cards");
+                JSONObject currObj = (JSONObject) jsonArray.get(0);
+                String cardType = (String) currObj.get("Type");
+                //System.out.println(cardType);
 
-        for (int i = 1; i < jsonArray.size(); ++i)
-        {
-            currObj = (JSONObject) jsonArray.get(i);
-            String cardContent = (String) currObj.get("Content");
-            String cardDescription = (String) currObj.get("Description");
-            String cardEffect = (String) currObj.get("Effect");
+                // loop file with array
+                for (int i = 1; i < jsonArray.size(); ++i)
+                {
+                    currObj = (JSONObject) jsonArray.get(i);
+                    String cardContent = (String) currObj.get("Content");
+                    String cardDescription = (String) currObj.get("Description");
+                    String cardEffect = (String) currObj.get("Effect");
 
-            Deck.AddCard(cardType, cardContent, cardDescription, cardEffect);
+                    Deck.AddCard(cardType, cardContent, cardDescription, cardEffect);
+                }
+            }
         }
-
+        
         Deck.printAllDecks();
     }
 }
