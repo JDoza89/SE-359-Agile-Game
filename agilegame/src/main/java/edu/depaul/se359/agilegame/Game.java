39382ec85e98c6c7009587671f2c21068feaf285
Ethan Lin <ethan.lin@uwex.edu>
2018-10-28 21:16:41 -0500

    console test purpose only

--- agilegame/src/main/java/edu/depaul/se359/agilegame/Game.java
+++ agilegame/src/main/java/edu/depaul/se359/agilegame/Game.java
@@ -1,7 +1,10 @@
 package edu.depaul.se359.agilegame;
 
 
-import edu.depaul.se359.agilegame.GameState.GameManager;
+
+import edu.depaul.se359.agilegame.Player.Player;
+import edu.depaul.se359.agilegame.Player.TeamManager;
+import edu.depaul.se359.agilegame.Utility.GameUtility;
 import org.json.simple.parser.*;
 import java.io.IOException;
 
@@ -9,7 +12,14 @@ public class Game {
 
     public static void main(String[] args) throws IOException, ParseException {
 
-        GameManager.getInstance().startGame();
+//        GameUtility.parseJSONtoDecks();
+//        TeamManager.getInstance().setNumberOfTeams(2);
+//        TeamManager.getInstance().setNumberOfPlayers(3);
+//        TeamManager.getInstance().createTeamsAndPlayers();
+//
+//        Player player = TeamManager.getInstance().getPlayer(3);
+//
+//        System.out.println(player.showHand().getHand());
 
     }
 
